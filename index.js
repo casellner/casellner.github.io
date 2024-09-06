b5325c5de2ef39092a68af195ed7700a5c7cca85
casellner <114782256+casellner@users.noreply.github.com>
2024-09-06 14:16:15 -0500

    add headers to index.html

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const c = canvas.getContext('2d'); //2d methods will be used
 canvas.width = 64 * 16; //1024
 canvas.height = 64 * 9; //576
 
-c.fillStyle = 'blue';
+c.fillStyle = `rgb(173, 216, 230)`; //canvas color
 c.fillRect(0, 0, canvas.width, canvas.height); //x, y, width, height
