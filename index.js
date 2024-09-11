ce053f4b9901a9f667f83e5a90e9baffbbf33a46
casellner <114782256+casellner@users.noreply.github.com>
2024-09-11 11:24:23 -0500

    temporarily change canvas dimensions for testing

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d'); //2d methods will be used
 
 //width and height from tutorial
-//canvas.width = 64 * 16; //1024
-//canvas.height = 64 * 9; //576
+canvas.width = 64 * 16; //1024
+canvas.height = 64 * 9; //576
 
 //fill document
-canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
+//canvas.width = document.body.clientWidth;
+//canvas.height = document.body.clientHeight;
 
 c.fillStyle = `rgb(173, 216, 230)`; //canvas color
 c.fillRect(0, 0, canvas.width, canvas.height); //x, y, width, height
